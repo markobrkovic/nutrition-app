1bf5f98d0d2d7e7d1d08b1356752b242603bce43
markobrkovic <querry999@gmail.com>
2021-10-06 12:54:06 +0200

    Added new component and finished styling it

---
+++ src/components/food-and-recipes/food-and-recipes.stories.js
@@ -0,0 +1,9 @@
+import "./food-and-recipes.css";
+import foodAndRecipes from "./food-and-recipes.html";
+
+export default {
+  title: "Components/Food and Recipes",
+  parameters: { layout: "centered" },
+};
+
+export const fruits = () => foodAndRecipes;
