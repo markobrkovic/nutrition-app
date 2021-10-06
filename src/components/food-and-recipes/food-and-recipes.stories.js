9849d367be29253f25b14518752c3882b8013574
markobrkovic <querry999@gmail.com>
2021-10-06 15:15:16 +0200

    Changed html to be more semantically correct and made slight CSS changes

--- src/components/food-and-recipes/food-and-recipes.stories.js
+++ src/components/food-and-recipes/food-and-recipes.stories.js
@@ -1,9 +1,11 @@
 import "./food-and-recipes.css";
-import foodAndRecipes from "./food-and-recipes.html";
+import foodAndRecipesFood from "./food-and-recipes-food.html";
+import foodAndRecipesSwitch from "./food-and-recipes-switch.html";
 
 export default {
   title: "Components/Food and Recipes",
   parameters: { layout: "centered" },
 };
 
-export const fruits = () => foodAndRecipes;
+export const filterFood = () => foodAndRecipesFood;
+export const filterSwitch = () => foodAndRecipesSwitch;
